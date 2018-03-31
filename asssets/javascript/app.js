19f77ed850a48c8e30739283a4797813e6a957d0
Abraham <abeis@msn.com>
2018-03-31 02:06:34 -0500

    testing posts

--- asssets/javascript/app.js
+++ asssets/javascript/app.js
@@ -1,13 +1,18 @@
 $(".fb-login-button").on('click',function(){
     FB.login(function(response){
-     if(response === 'connected'){
-       alert("login worked");
-     }else if(response === 'not_authorized'){
-       alert('You are allready logged in');
-     }else{
-         alert('Totaly not logged in')
-     }
-    })
-   } );
+        if(response === 'connected'){
+        alert("login worked");
+        }else if(response === 'not_authorized'){
+        alert('You are allready logged in');
+        }else{
+            alert('Totaly not logged in')
+        }
+    });
+
+    FB.api('/me', {fields: 'last_name'}, function(response) {
+        console.log(response);
+      });
+
+
+});
       
-   
