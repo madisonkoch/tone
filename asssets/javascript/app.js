e6bf7800435b5f0471a1cce8be02cee4d1949d6c
Abraham <abeis@msn.com>
2018-03-31 02:16:32 -0500

    testing name

--- asssets/javascript/app.js
+++ asssets/javascript/app.js
@@ -20,7 +20,8 @@ $('.test-btn').on('click', function(){
         $('body').append(response);
         console.log(response);
       });
-
+      
+      var body = 'Reading JS SDK documentation';
       FB.api('/me/feed', 'post', { message: body }, function(response) {
         if (!response || response.error) {
           alert('Error occured');
