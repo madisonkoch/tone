e880a8c5c4d6c5600897edbab2014e27652bf3a9
Abraham <abeis@msn.com>
2018-03-31 11:46:13 -0500

    posts

--- asssets/javascript/app.js
+++ asssets/javascript/app.js
@@ -16,17 +16,14 @@ $(".fb-login-button").on('click',function(){
       
 $('.test-btn').on('click', function(){
     $('body').append('test');
-    FB.api('/me', {fields: 'last_name'}, function(response) {
-        $('body').append(response);
-        console.log(response);
-      });
-      // for this next step to work need to ste up auth token
-      var body = 'Testing';
+  
       FB.api( '/user-id/feed', 'post', { message: body }, function(response) {
-       
-        
+          console.log(response)
+        if (!response || response.error) {
+          alert('Error occured');
+        } else {
           alert('Post ID: ' + response.id);
-        
+        }
       });
 
 })
