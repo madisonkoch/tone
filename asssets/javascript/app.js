86a12b26396d62497a1ef2cfbbaf86ceae766b85
Abraham <abeis@msn.com>
2018-04-01 18:15:10 -0500

    fb api changes

--- asssets/javascript/app.js
+++ asssets/javascript/app.js
@@ -18,39 +18,13 @@ $('.test-btn').on('click', function(){
     $('body').append('test');
   
     FB.api(
-        "/page-id/feed",
-        function (response) {
-            console.log(response)
-          if (response && !response.error) {
-            /* handle the result */
-          }
+        '/me',
+        'GET',
+        {},
+        function(response) {
+           console.log(response)
         }
-    );
+      );
+      
 
 })
-
-//let example = userInput;
-
-/*$ curl -H "Content-Type: application/json" --data \
-    '{comment: {text: "what kind of idiot name is foo?"},
-      languages: ["en"],
-      requestedAttributes: {TOXICITY:{}} }' \
-    https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=AIzaSyCY4GjmWVO7suPMSdD-V-Pm8tlExBkIFJE*/
-
-    $.ajax({
-        contentType: "application/json",
-        data: JSON.stringify({
-                comment: {
-                        text: "what a lovely hat"
-                },
-                languages: ["en"],
-                requestedAttributes: {
-                        TOXICITY: {}
-                }
-        }),
-        method: 'POST',
-        url: 'https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=AIzaSyC_mGbSsEJnpL8tD7BnO5jRXS_uTPMyFwE',
-        success: function(response) {
-                console.log(response);
-        }
-});
