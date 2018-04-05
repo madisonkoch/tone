853a94bf01fb677ebee3cae6aba775f13f51988d
Abraham <abeis@msn.com>
2018-04-04 21:00:32 -0500

    test

--- api.js
+++ api.js
@@ -16,18 +16,18 @@ express()
 //   .get('/', (req, res) => res.render('index.html'))
 //   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
  
-// app.post('/hello', function (req, res, next) {
-//   var userName = req.body.user_name;
-//   var botPayload = {
-//     text : 'Hello ' + userName + ', welcome to the Channel'
-//   };
-//   // Loop otherwise..
-//   if (userName !== 'slackbot') {
-//     return res.status(200).json(botPayload);
-//   } else {
-//     return res.status(200).end();
-//   }
-// });
+app.post('/hello', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello ' + userName + ', welcome to the Channel'
+  };
+  // Loop otherwise..
+  // if (userName !== 'slackbot') {
+  //   return res.status(200).json(botPayload);
+  // } else {
+  //   return res.status(200).end();
+  // }
+});
  
 // app.listen(8080, function () {
 //   console.log('Listening on port ' + port);
