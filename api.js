580cb9e851aa37fd9fffa6e554a4421dfa0eb48c
Abraham <abeis@msn.com>
2018-04-04 21:04:25 -0500

    test

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 
 express()
    .use(express.static(path.join(__dirname, './')))
-  .get('/', (req, res) => res.render('/index.html'))
+  .get('/', (req, res) => res.render('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 // const express = require('express')
 // const path = require('path')
