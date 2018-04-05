38035308e5706953af4bac9be61d42c59c8e10fb
Abraham Anderson <abeis@msn.com>
2018-04-05 16:15:42 -0500

    test

--- api.js
+++ api.js
@@ -44,22 +44,5 @@ app
 
 
   app.get('/auth/redirect', (req, res) =>{
-    var options = {
-        uri: 'https://slack.com/api/oauth.access?code='
-            +req.query.code+
-            '&client_id='+process.env.CLIENT_ID+
-            '&client_secret='+process.env.CLIENT_SECRET+
-            '&redirect_uri='+process.env.REDIRECT_URI,
-        method: 'GET'
-    }
-    request(options, (error, response, body) => {
-        var JSONresponse = JSON.parse(body)
-        if (!JSONresponse.ok){
-            console.log(JSONresponse)
-            res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
-        }else{
-            console.log(JSONresponse)
-            res.send("Success!")
-        }
-    })
+    res.send("code " + req.params.code);
 })
