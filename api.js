0156d5b5a27afc52f29c2bd2f73522baa08094a1
Abraham <abeis@msn.com>
2018-04-04 20:54:27 -0500

    test

--- api.js
+++ api.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
+var path = require('path');
 
-express()
-.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index.html'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+
+// express()
+// .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('index.html'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
  
 // app.post('/hello', function (req, res, next) {
 //   var userName = req.body.user_name;
