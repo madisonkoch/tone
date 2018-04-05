2c85902f82e3325cc610ff7af80f36465c01d6cf
Abraham Anderson <abeis@msn.com>
2018-04-05 14:19:05 -0500

    test

--- api.js
+++ api.js
@@ -9,7 +9,7 @@ app.post('/hello', function (req, res) {
   var botPayload = {
     text : 'Hello, ' + req + '!'
   };
-  res.status(200).json(req);
+  res.status(200).json(botPayload);
   // avoid infinite loop
   // if (userName !== 'slackbot') {
   
@@ -23,26 +23,8 @@ app
    .use("/styles", express.static(__dirname))
   .get('/', (req, res) => res.render('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
 
-// express()
-// .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('index.html'))
-//   .l isten(PORT, () => console.log(`Listening on ${ PORT }`))
- 
 
-  // Loop otherwise..
-  // if (userName !== 'slackbot') {
-  //   return res.status(200).json(botPayload);
-  // } else {
-  //   return res.status(200).end();
-  // }
-// });
- 
-// app.listen(PORT, function () {
-//   console.log('Listening on port ' + port);
-// });
+  app.post('/auth' , function(req, res){
+     res.render('auth.html')
+  })
