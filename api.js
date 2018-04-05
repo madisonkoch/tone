69836181ca388757d2a7fbc5a446128f9e1096c4
Abraham <abeis@msn.com>
2018-04-04 19:43:51 -0500

    etst

--- api.js
+++ api.js
@@ -4,24 +4,22 @@ var path = require('path');
 console.log('I launched');
  
 var app = express();
-var port = process.env.PORT || 1337; 
+var port = process.env.PORT || 8080; 
  
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
  
-// test route
-app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
- 
+
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/fbContent.html'));
 });
 
 
 app.post('/hello', function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
-    text : 'Hello ' + userName + ', welcome to Devdactic Slack channel! I\'ll be your guide.'
+    text : 'Hello ' + userName + ', welcome to the Channel'
   };
   // Loop otherwise..
   if (userName !== 'slackbot') {
