92ab561c6d16abefb89e584b065b2d0b6cb5b21a
Abraham <abeis@msn.com>
2018-04-04 20:50:58 -0500

    test

--- api.js
+++ api.js
@@ -1,20 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-console.log('I launched');   
- 
-var app = express();
-var port = process.env.PORT || 8080; 
- //123
-// body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
- 
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
-
+express()
+.use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index.html'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
  
 // app.post('/hello', function (req, res, next) {
 //   var userName = req.body.user_name;
