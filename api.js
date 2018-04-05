f06e8ab99b4bf108d457a548629e439623c12828
Abraham <abeis@msn.com>
2018-04-04 19:48:57 -0500

    test

--- api.js
+++ api.js
@@ -1,34 +1,34 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-console.log('I launched');
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var path = require('path');
+// console.log('I launched');
  
-var app = express();
-var port = process.env.PORT || 8080; 
+// var app = express();
+// var port = process.env.PORT || 8080; 
  
-// body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
+// // body parser middleware
+// app.use(bodyParser.urlencoded({ extended: true }));
  
 
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/index.html'));
+// });
 
 
-app.post('/hello', function (req, res, next) {
-  var userName = req.body.user_name;
-  var botPayload = {
-    text : 'Hello ' + userName + ', welcome to the Channel'
-  };
-  // Loop otherwise..
-  if (userName !== 'slackbot') {
-    return res.status(200).json(botPayload);
-  } else {
-    return res.status(200).end();
-  }
-});
+// app.post('/hello', function (req, res, next) {
+//   var userName = req.body.user_name;
+//   var botPayload = {
+//     text : 'Hello ' + userName + ', welcome to the Channel'
+//   };
+//   // Loop otherwise..
+//   if (userName !== 'slackbot') {
+//     return res.status(200).json(botPayload);
+//   } else {
+//     return res.status(200).end();
+//   }
+// });
 
-app.listen(8080, function () {
-  console.log('Listening on port ' + port);
-});
+// app.listen(8080, function () {
+//   console.log('Listening on port ' + port);
+// });
