d54cab3a9f2a3b5453813e1d2010ae505eb5be07
Abraham Anderson <abeis@msn.com>
2018-04-05 16:31:25 -0500

    test

--- api.js
+++ api.js
@@ -43,6 +43,31 @@ app
   }
 
 
-  app.get('/auth/redirect', (req, res) =>{
-   res.send('user ' + req.param() );
-})
+//   app.get('/auth/redirect', (req, res) =>{
+//    res.send('user ' + req.param() );
+// })
+
+/* WAIT FOR NEW APP INSTALLATION REQUESTS FROM SLACK */
+app.get('/auth/redirect', function(req, res) {
+  // Tease out accessCode from the Slack request, if it exists 
+  var url = req.url;
+  var codePos = url.indexOf("code="); //index where code= starts in url
+  var codeStartPos = codePos + 5; //Start of accessCode (+5 because code= is 5 characters)
+  var endingPos = url.indexOf("&"); //End of accessCode, where another parameter starts
+  var accessCode = url.substring(codeStartPos, endingPos).toString();   //Extract code from url
+
+  // Verify user accepted Slack's auth request by looking for access_code existence
+  if (codePos > -1) {    // User authorized oAuth request from Slack
+    var completePath = path + accessCode; //Slack API call + code to receive accessToken and teamInfo
+    request(completePath, function(error, response, body) { // Request token from Slack using the access_code, then handle response
+      if(!error && response.statusCode == 200 && teamInfo.ok == true){
+        var teamInfo = JSON.parse(body);   //Slack sends back access_code and team info in a JSON object
+        //SAVE THE ACCESS_CODE
+        req.send('acesses code ' + accessCode)
+      } else {
+        //ERROR
+      }
+     });
+   } else {          //User denied auth request from Slack, so reroute back to signup page to start over
+    //REROUTE USER BACK TO INSTALL PAGE, THEY DENIED AUTH REQUEST
+   }
