8c585a87eef917115086b46555aea634ce9865f0
Abraham <abeis@msn.com>
2018-04-04 21:31:40 -0500

    test

--- api.js
+++ api.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require("express");
+var app     = express();
+app.use(express.static(__dirname + '/View'));
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/Script'));
+//Store all JS and CSS in Scripts folder.
 
-express()
-   .use(express.static(path.join(__dirname, './index.html')))
-  
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+  //It will find and locate index.html from View or Scripts
+});
+
+app.get('/about',function(req,res){
+  res.sendFile('/about.html');
+});
+
+app.get('/sitemap',function(req,res){
+  res.sendFile('/sitemap.html');
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
 // const express = require('express')
 // const path = require('path')
 // const PORT = process.env.PORT || 5000
