745b43e3f779900718aafc1eb635ea08d4dbbb71
Abraham Anderson <abeis@msn.com>
2018-04-06 15:32:21 -0500

    setup slack uri

--- asssets/javascript/app.js
+++ asssets/javascript/app.js
@@ -99,7 +99,7 @@ $('.event').on('click', function(){
     $.ajax({
   
         dataType: 'json',
-        url:'https://api.eventful.com/rest/events/search?app_key=hBd8FWcZ33KgBkBC&where=32.746682,-117.162741&within=25',
+        url:'https://api.eventful.com/json/events/search?app_key=hBd8FWcZ33KgBkBC&where=32.746682,-117.162741&within=25',
         method:'GET',
         success:function(data){console.log(data)},
         error:function(data){console.log(data)}
