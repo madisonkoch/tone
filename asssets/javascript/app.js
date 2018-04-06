e1b3b77cbb1061fcaeaa7752d203feee0bdad0c3
Abraham Anderson <abeis@msn.com>
2018-04-06 16:10:27 -0500

    test

--- asssets/javascript/app.js
+++ asssets/javascript/app.js
@@ -97,7 +97,7 @@
 $('.event').on('click', function(){
     alert('test')
     $.ajax({
-        dataType: 'xml ',
+        dataType: 'JSONP text xml ',
         url:'https://api.eventful.com/rest/events/search?app_key=hBd8FWcZ33KgBkBC&where=32.746682,-117.162741&within=25',
         method:'GET',
         success:function(data){console.log(data)},
