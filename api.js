23f5a852ecfc0eb2a7914f89ddc4e0b4a70391b7
Abraham <abeis@msn.com>
2018-04-07 12:29:28 -0500

    added variable to save and redner indexhtml

--- api.js
+++ api.js
@@ -75,7 +75,7 @@ app.get('/auth/redirect', (req, res) =>{
           res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
       }else{
           console.log(JSONresponse)
-          res.sendFile('index.html');
+          res.sendFile('index');
           savedThing = JSONresponse;
       }
   })
